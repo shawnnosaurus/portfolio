116814466e87b1ff243670dd52cd8a59d39670f3
Shawn Genlloud <shawn.genlloud@derivco.co.za>
2021-08-26 19:33:10 +0200

    ️♻️💄Refactor Styling
    Correct the BEM naming convention.
    Enlarge the max width on desktop.
    Capitalise the CV icon, to center better.
    Move scrolling back to main page, as it didn't help with the stuttering.
    Implement a simpler and smoother menu toggle.
    Add animated background to the intro section.
    Add CV for download.
    Trim JS nav toggle.

--- Portfolio/assets/js/nav.js
+++ Portfolio/assets/js/nav.js
@@ -1,12 +1,4 @@
 (function () {
-  const body = document.body,
-    toggleMenu = _ =>
-      body.hasAttribute('menu-open') ?
-        body.removeAttribute('menu-open') :
-        body.setAttribute('menu-open', '');
-
-  document.querySelectorAll('.hamburger')
-    .forEach(btn => btn.addEventListener('click', toggleMenu));
-  document.querySelector('#menu')
-    .addEventListener('click', toggleMenu);
+  document.querySelector('.page + aside')
+    .addEventListener('click', e => e.currentTarget.classList.toggle('menu-open'));
 }());
