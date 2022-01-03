8c48b456f9ea173dc983ba110fc7aed76da07fd5
Shawn Genlloud <shawn.genlloud@derivco.co.za>
2022-01-03 09:45:03 +0200

    ✨Touch Screen Captions

--- Portfolio/assets/js/nav.js
+++ Portfolio/assets/js/nav.js
@@ -7,15 +7,45 @@ new (class {
     return this.document.querySelector(".page + aside");
   }
 
+  get portfolioLinks() {
+    return this.document.querySelectorAll(".portfolio__link");
+  }
+
+  get portfolioLinkCaptions() {
+    return this.document.querySelectorAll(".portfolio__link figcaption");
+  }
+
+  get isTouchDevice() {
+    return (('ontouchstart' in window) ||
+      (navigator.maxTouchPoints > 0) ||
+      (navigator.msMaxTouchPoints > 0));
+  }
+
   constructor(global) {
     this.global = global;
+
     this.addEventListeners();
   }
 
   addEventListeners() {
     this.menu.addEventListener("click", this.toggleMenuOpen);
+
+    this.portfolioLinks.forEach(pl =>
+      pl.addEventListener('click', this.doubleTapCaptionsOnMobile));
   }
 
-  toggleMenuOpen = (event) =>
-    event.currentTarget.classList.toggle("menu--open");
+  toggleMenuOpen = event => event.currentTarget.classList.toggle("menu--open");
+
+  doubleTapCaptionsOnMobile = event => {
+    const captionComponent = event.currentTarget.querySelector('figcaption');
+    this.portfolioLinkCaptions.forEach(plc =>
+      plc !== captionComponent ? plc.style.opacity = '' : '');
+
+    if (this.isTouchDevice) {
+      const captionHidden = !captionComponent.style.opacity;
+
+      captionComponent.style.opacity = captionHidden ? '1' : '0';
+      if (captionHidden) event.preventDefault();
+    }
+  };
 })(window);
