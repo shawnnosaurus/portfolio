a1a9753803a086dc8ce04349e6c7db371d2ceb4b
Shawn Genlloud <shawn.genlloud@derivco.co.za>
2022-01-07 09:08:37 +0200

    Add Chat & Analytics to all Pages

--- Portfolio/assets/js/drift_chat.js
+++ Portfolio/assets/js/drift_chat.js
@@ -1,9 +1,26 @@
 "use strict";
 
-!function () {
-  var t = window.driftt = window.drift = window.driftt || [];
+!function (global) {
+  global.document.body.innerHTML += `
+    <div 
+      class="drift-widget-placeholder" 
+      title="Loading chat..." 
+      aria-hidden="true" 
+      tabindex="-1" 
+      style="background:#5c6282;opacity:0.25;position:fixed;right:35px;bottom:35px;padding:15px;border-radius:50%"
+    >
+      <svg width="25" height="23" viewBox="0 0 25 23">
+        <path 
+          d="M24.516 9.953C24.516 4.453 19.04 0 12.258 0 5.476 0 0 4.452 0 9.953c0 3.318 1.986 6.24 5.05 8.053-.34 2.552-1.815 4.055-1.844 4.084-.14.14-.17.368-.113.567a.524.524 0 0 0 .482.312c2.95 0 5.335-1.93 6.612-3.206.652.086 1.362.142 2.07.142 6.783 0 12.26-4.452 12.26-9.953z" 
+          fill="#ffffff"
+        ></path>
+      </svg>
+    </div>
+  `;
+
+  var t = global.driftt = global.drift = global.driftt || [];
   if (!t.init) {
-    if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
+    if (t.invoked) return void (global.console && console.error && console.error("Drift snippet included twice."));
     t.invoked = !0, t.methods = ["identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on"],
       t.factory = function (e) {
         return function () {
@@ -13,12 +30,12 @@
       }, t.methods.forEach(function (e) {
         t[e] = t.factory(e);
       }), t.load = function (t) {
-        var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
+        var e = 3e5, n = Math.ceil(new Date() / e) * e, o = global.document.createElement("script");
         o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
-        var i = document.getElementsByTagName("script")[0];
+        var i = global.document.getElementsByTagName("script")[0];
         i.parentNode.insertBefore(o, i);
       };
   }
-}();
-drift.SNIPPET_VERSION = '0.3.1';
-drift.load('gmpbtgvzs5ca');
+  drift.SNIPPET_VERSION = '0.3.1';
+  drift.load('gmpbtgvzs5ca');
+}(window);
